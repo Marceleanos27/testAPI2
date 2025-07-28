0c74ec48a9909901532dc678be439ad060657f24
Marceleanos27 <lehocky.business@gmail.com>
2025-07-28 07:55:30 +0200

    Update chatbot-widget.js

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -3,7 +3,7 @@
   window.__marcelChatbotLoaded = true;
 
   const iframe = document.createElement("iframe");
-  iframe.src = "https://chatbotmarcel.vercel.app/"; // ⬅️ Change to your real URL (e.g. Vercel)
+  iframe.src = "https://test-api-2-nmprbscj0-marcelitos-projects.vercel.app/"; // ⬅️ Change to your real URL (e.g. Vercel)
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
